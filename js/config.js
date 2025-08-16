4ce3e993868836521fc2c2da01273ff262d93f40
AKM-SCHNEIDER <114222944+AKM-SCHNEIDER@users.noreply.github.com>
2025-08-16 13:29:12 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_3xH6d1Yyi', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7b65r77s0erqg1sjeo5op615qc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
